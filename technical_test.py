9be77f2f90eacfbf53f04b74717fb2b1731f0941
Nakul Pant <145569699+Nakul-Pant-DS@users.noreply.github.com>
2024-01-14 23:18:41 +0530

    Add files via upload

---
+++ technical_test.py
@@ -0,0 +1,30 @@
+"""
+Technical Data Analyst - ESG Research, ESG Book
+ 
+Please complete this test within 3 days of receiving it,
+using Python version 3.8 or higher.
+ 
+To complete this test you will need the sample_data_100.csv,
+referred to from this point on simply as 'the data'.
+The data relates to a collection of companies, with annual emissions metrics. 
+Note that this is mockup data, so not actual figures.
+ 
+The purpose of this test is to assess your data science skills and
+your ability to document code well. The questions outlined below are
+the bare minimum required, we invite you to take initiative beyond these
+to derive further insights from the data provided. In addition the code
+does not need to be constrained to this .py file, you may structure your
+code across multiple files.
+ 
+SUBMISSION: please reply to email with the python scripts / packages you develop
+attached in a zip, including a folder with any outputs, e.g. plots
+"""
+ 
+ 
+# Q1 - Data often has errors, write code to asses, handle and clean the data
+ 
+ 
+# Q2 - Derive insights from the data and provide plots of your findings
+ 
+ 
+# Q3 - Productionise your code - structure, document and test the code
